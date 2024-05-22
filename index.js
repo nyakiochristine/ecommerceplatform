4852cae8281645f58f1725adc7e3a9ce52decce0
christine <christine.mwangi@student.moringaschool.com>
2024-05-23 00:56:38 +0300

    created a simple landing page for an ecommerce website

---
+++ index.js
@@ -0,0 +1,34 @@
+const image= document.getElementById('productImg');
+const btn=document.getElementsByClassName('btn');
+
+
+btn[0].addEventListener('click', function(){
+    image.src='images/Crochet Patterns For Your New Bags (34 Pics).jpeg';
+    for(bt of btn){
+        bt.classList.remove('active');
+            }
+            this.classList.add('active');
+
+
+});
+
+btn[1].addEventListener('click', function(){
+    image.src='images/Crochet Patterns For Your New Bags (34 Pics).jpeg';
+    for(bt of btn){
+        bt.classList.remove('active');
+            }
+            this.classList.add('active');
+
+
+});
+
+btn[2].addEventListener('click', function(){
+    image.src='images/Granny Square Tote Bag · Hopelessly Devoted Crochet.jpeg';
+    for(bt of btn){
+        bt.classList.remove('active');
+            }
+            this.classList.add('active');
+
+
+});
+
