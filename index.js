491750607c420b2f1023f9c1811169d0d16ff02e
christine <christine.mwangi@student.moringaschool.com>
2024-05-30 01:21:45 +0300

    ft-added the logic for success message after user adds items to the cart

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ const image= document.getElementById('productImg');
 const btn=document.getElementsByClassName('btn');
 
 
+let selectedColor = '';
+let selectedSize = '';
+
+
+const colorLabels = document.querySelectorAll('.select-color label');
+colorLabels.forEach((label) => {
+  label.addEventListener('click', function() {
+    selectedColor = label.querySelector('input').id;
+  });
+});
+
+// Size selection
+const sizeLabels = document.querySelectorAll('.select-size label');
+sizeLabels.forEach((label) => {
+  label.addEventListener('click', function() {
+    selectedSize = label.querySelector('input').id;
+  });
+});
+
+
 btn[0].addEventListener('click', function(){
     image.src='images/Crochet Patterns For Your New Bags (34 Pics).jpeg';
     for(bt of btn){
@@ -30,5 +50,23 @@ btn[2].addEventListener('click', function(){
             this.classList.add('active');
 
 
+});
+
+//message after adding to cart
+
+const submitButton = document.querySelector('.submit');
+const successMessage = document.getElementById('success-message');
+
+submitButton.addEventListener('click', function() {
+  // Add your logic to add the product to the cart here
+  // ...
+
+  // Display the success message
+  successMessage.style.display = 'block';
+
+  // Hide the success message after 4 seconds
+  setTimeout(function() {
+    successMessage.style.display = 'none';
+  }, 4000);
 });
 
